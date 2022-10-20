13d353f807bc13f12c848163a21dc7e812e489db
Philippe Degen <phdegen@hotmail.com>
2022-10-20 15:42:23 +0200

    mb: fix switch break bug and adapt expected range to 220km

--- vehicle/OVMS.V3/components/vehicle_mercedesb250e/src/vehicle_mercedesb250e.cpp
+++ vehicle/OVMS.V3/components/vehicle_mercedesb250e/src/vehicle_mercedesb250e.cpp
@@ -218,7 +218,7 @@ void OvmsVehicleMercedesB250e::IncomingFrameCan1(CAN_frame_t* p_frame)
         StandardMetrics.ms_v_bat_range_full->SetValue(mt_mb_bat_capacity->AsFloat() / consumption);
       }
       if (range < 2047)
-        StandardMetrics.ms_v_bat_soc->SetValue((float)range / 240.0 * 100.0);
+        StandardMetrics.ms_v_bat_soc->SetValue((float)range / 220.0 * 100.0);
       break;
     }
   case 0x3eb: 
