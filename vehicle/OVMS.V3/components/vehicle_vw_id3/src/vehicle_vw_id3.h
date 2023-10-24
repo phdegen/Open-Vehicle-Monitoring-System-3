2ef30d831a734724f82870ce9d736f129204be08
Philippe Degen <phdegen@hotmail.com>
2023-10-24 14:33:08 +0200

    vehicle id3: multi-packet data for VIN

--- vehicle/OVMS.V3/components/vehicle_vw_id3/src/vehicle_vw_id3.h
+++ vehicle/OVMS.V3/components/vehicle_vw_id3/src/vehicle_vw_id3.h
@@ -73,7 +73,7 @@ class OvmsVehicleVWID3 : public OvmsVehicle {
     
   protected:
     string id3_obd_rxbuf;
-    char id3_vin[5] = "";
+    char id3_vin[15] = "";
 };
 
 #endif //#ifndef __VEHICLE_VW_ID3_H__
